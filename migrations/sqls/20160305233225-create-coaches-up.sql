0dd3d2881a298ff75d7eb60a9f48062f4b116b3f
David Hansen <hansede@gmail.com>
2016-03-07 21:52:24 -0700

    Added avatars. Changed coaches.

--- migrations/sqls/20160305233225-create-coaches-up.sql
+++ migrations/sqls/20160305233225-create-coaches-up.sql
@@ -3,5 +3,6 @@ CREATE TABLE coaches (
   name varchar(40) NOT NULL,
   email varchar(40) NOT NULL,
   phone varchar(40) NOT NULL,
+  avatar varchar(40) NOT NULL,
   UNIQUE(email)
 )
--- migrations/sqls/20160306220557-sample-coaches-up.sql
+++ migrations/sqls/20160306220557-sample-coaches-up.sql
@@ -1,20 +1,15 @@
-insert into coaches (name, email, phone)
+insert into coaches (name, email, phone, avatar)
 values (
-  'Dr. Strangelove',
-  'strangelove@gmail.com',
-  '(555) 555-5555'
+  'Dr. Who',
+  'who@gmail.com',
+  '(555) 777-7777',
+  '/img/who.png'
 );
 
-insert into coaches (name, email, phone)
-values (
-  'Dr. House',
-  'house@gmail.com',
-  '(555) 455-4455'
-);
-
-insert into coaches (name, email, phone)
+insert into coaches (name, email, phone, avatar)
 values (
   'Dr. Watson',
   'watson@gmail.com',
-  '(555) 333-7733'
+  '(555) 123-4567',
+  '/img/watson.png'
 );
