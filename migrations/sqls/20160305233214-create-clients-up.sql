ed863d52d30a2ef0b52996c79119625382540ea4
David Hansen <hansede@gmail.com>
2016-03-12 16:38:37 -0700

    Reduced size of google_id fields.

--- migrations/sqls/20160305233214-create-clients-up.sql
+++ migrations/sqls/20160305233214-create-clients-up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION pgcrypto;
 
 CREATE TABLE clients (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  google_id varchar(1200) NOT NULL,
+  google_id varchar(25) NOT NULL,
   name varchar(40) NOT NULL,
   email varchar(40) NOT NULL,
   avatar varchar(300),
--- migrations/sqls/20160305233225-create-coaches-up.sql
+++ migrations/sqls/20160305233225-create-coaches-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE coaches (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  google_id varchar(1200) NOT NULL,
+  google_id varchar(25) NOT NULL,
   name varchar(40) NOT NULL,
   email varchar(40) NOT NULL,
   phone varchar(40) NOT NULL,
